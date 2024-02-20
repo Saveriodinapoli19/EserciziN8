0747e04b9b7767e9e2bfb5371c1cff11f3f0d9c4
saverio <saveriodinapoli99@gmail.com>
2024-02-20 20:05:49 +0100

    commitRepo

--- src/main/java/com/example/EsercizioN8/CarRepository.java
+++ src/main/java/com/example/EsercizioN8/CarRepository.java
@@ -1,6 +1,8 @@
 package com.example.EsercizioN8;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
 }
